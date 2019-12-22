840d65c702a436541edfc69f30f1029f7ef221b5
amit016verma@gmail.com <amit016verma@gmail.com>
2019-12-22 16:17:15 +0530

    Alertdialog implemented with the activity

--- app/src/main/java/com/amit/verma/activitylifecycleexample/MainActivity.kt
+++ app/src/main/java/com/amit/verma/activitylifecycleexample/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.amit.verma.activitylifecycleexample
 
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.util.Log
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -16,16 +18,42 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.e(TAG, "onCreate() called")
 
-        button.also {
-            it.setOnClickListener {
-                val intent = Intent(this, Main2Activity::class.java)
-                startActivity(intent)
-                finish()
-            }
+
+        button.setOnClickListener {
+            val intent = Intent(this, Main2Activity::class.java)
+            startActivity(intent)
         }
 
-        Log.e(TAG, "onCreate() called")
+
+        // when button is clicked, show the alert
+        btn_dialog.setOnClickListener {
+            // build alert dialog
+            val dialogBuilder = AlertDialog.Builder(this)
+
+            // set message of alert dialog
+            dialogBuilder.setMessage("Do you want to close this application ?")
+                // if the dialog is cancelable
+                .setCancelable(false)
+                // positive button text and action
+                .setPositiveButton("Proceed", DialogInterface.OnClickListener { dialog, id ->
+                    finish()
+                })
+                // negative button text and action
+                .setNegativeButton("Cancel", DialogInterface.OnClickListener { dialog, id ->
+                    dialog.cancel()
+                })
+
+            // create dialog box
+            val alert = dialogBuilder.create()
+            // set title for alert dialog box
+            alert.setTitle("AlertDialogExample")
+            // show alert dialog
+            alert.show()
+        }
+
+
     }
 
     override fun onStart() {
